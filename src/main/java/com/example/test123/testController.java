62a4a048395a37878e9584b6d9f3ba6ad3173789
WonHwiAhn <myroom123@bible.ac.kr>
2020-10-26 22:59:39 +0900

    first commit

---
+++ src/main/java/com/example/test123/testController.java
@@ -0,0 +1,12 @@
+package com.example.test123;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+    @GetMapping
+    public void test(@Vali) {
+
+    }
+}
